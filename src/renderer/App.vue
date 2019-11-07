a9fd02f99595c7cf3a6c1a553a354b58657cdb34
Secr3ts <42939294+Secr3ts@users.noreply.github.com>
2019-11-07 19:36:24 +0100

    ini

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'solo'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
